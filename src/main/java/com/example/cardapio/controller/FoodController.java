6021b73388da83b932fb0406fd57f9825f2077dc
Alisson Rodrigues <alissonrodrigues565656@gmail.com>
2023-05-03 23:52:32 -0300

    feat: get all foods

--- src/main/java/com/example/cardapio/controller/FoodController.java
+++ src/main/java/com/example/cardapio/controller/FoodController.java
@@ -1,14 +1,22 @@
 package com.example.cardapio.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.cardapio.food.Food;
+import com.example.cardapio.food.FoodRepository;
+import com.example.cardapio.food.FoodRequestDTO;
+import com.example.cardapio.food.FoodResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("food")
 public class FoodController {
     @GetMapping
-    public void getAll(){
+    public List<FoodResponseDTO> getAll(){
 
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
+
+        return foodList;
     }
 }
