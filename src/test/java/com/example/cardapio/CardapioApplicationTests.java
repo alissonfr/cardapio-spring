a3d84e9a5e25d2c0b46e6fd4cc7f0a3174fd4260
Alisson Rodrigues <alissonrodrigues565656@gmail.com>
2023-05-03 22:34:28 -0300

    initial commit

---
+++ src/test/java/com/example/cardapio/CardapioApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.cardapio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioApplicationTests {
+
+
+
+}
